8872c1272b92ef2b2d9e7ba0509cfc583b07fc95
ilhamfauzan <31692409+ilhamfauzan@users.noreply.github.com>
2024-12-14 19:19:54 +0700

    footer link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="bg-white border-t border-gray-200 shadow-sm py-6">
     <div class="max-w-7xl mx-auto text-center px-4">
-        <p class="text-sm text-gray-500">10122223 - Ilham Fauzan - IF6</p>
+        <p class="text-sm text-gray-500">10122223 - <a class="underline" href="https://zan.re" target="_blank">Ilham Fauzan</a> - IF6
+        </p>
     </div>
 </footer>
