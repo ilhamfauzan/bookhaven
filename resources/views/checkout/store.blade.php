7e50333cdf0465617e99260736606ae1a68ff958
ilhamfauzan <31692409+ilhamfauzan@users.noreply.github.com>
2024-12-15 09:27:56 +0700

    fix shadow

--- resources/views/checkout/store.blade.php
+++ resources/views/checkout/store.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="overflow-hidden">
                 
                 <div class="p-6">
                     <div class="flex items-center justify-center">
